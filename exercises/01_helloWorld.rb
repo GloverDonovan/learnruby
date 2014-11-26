ce14a7c36b0b85bd76e5bd2e3aef7bc367a8edac
GloverDonovan <ultimatedonovan@gmail.com>
2014-11-26 18:02:54 +0000

    add first 4 exercises

---
+++ exercises/01_helloWorld.rb
@@ -0,0 +1,12 @@
+puts "Hello World"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+
+puts "Oh hey, my own string!"
+puts "Haha. Learning ruby the hard way."
+
+
